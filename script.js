c274ab0147176ea787ef6064f4b68c2e358e88ff
Caioiuri1 <caioiuri1127@gmail.com>
2025-04-07 14:24:03 -0300

    a

--- script.js
+++ script.js
@@ -17,18 +17,18 @@ iniciofechar.addEventListener('click', () => {
 })
 
 function enviarwhats(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     const nome = document.getElementById('firstname').value;
     const email = document.getElementById('email').value;
     const tel = document.getElementById('number').value;
     const mensagem = document.getElementById('text').value;
-    const telefone = '558791174227'
+    const telefone = '558791174227';
 
-    const texto = `Ola! Me chamo ${nome}, ${mensagem}. Esse é o meu email ${email} e o meu número de telefone ${tel}`
+    const texto = `Ola! Me chamo ${nome}, ${mensagem}. Esse é o meu email ${email} e o meu número de telefone ${tel}`;
     const msgFormatada = encodeURIComponent(texto)
 
-    const url = `https://wame.me/${telefone}?text=${msgFormatada}`
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
 
-    window.open(url, '_blank')
+    window.open(url, '_blank');
 }
